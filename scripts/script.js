a137c6423e5b9c2576a26da5edb39a0d33d500e6
Hardik Gupta <92462575+hadik10@users.noreply.github.com>
2022-11-07 22:58:35 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){               
+         $("#sendName").click(function () {
+    //console.log($("#fname").val());
+    $("#Start_1").text("Your information" + $("#User_Name ").val() + $("#Dept_t    ").val() + $("#email_id  ").val() + $("#text_area").val()+ $("#rad_1").val() +$("#rad_2").val() + $("#movie_1").val() +$("#check_box1").val());
+  });    
+                  });
